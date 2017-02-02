43b23c0f159d788ef164e0e3cce18306989043af
Ramon Ramos <ramonramospaz@gmail.com>
2017-02-01 23:18:41 -0400

    Se agrega texto informativo a la ruta de mis servicios online

--- template-productos.php
+++ template-productos.php
@@ -8,6 +8,12 @@ session_start();
 		<div class="wrapper">
 	
 			<h1><?php the_title(); ?></h1>
+			<?php if(qtranxf_getLanguage()=="es"){ ?>
+				<article style="text-align: center"><div>Obtenga vía online y en tiempo real, los más profesionales planes estratégicos y diagnósticos situacionales, a bajos precios y adaptados a las necesidades de su marca o producto.</div></article>
+			<?php } ?>
+			<?php if(qtranxf_getLanguage()=="en"){ ?>
+				<article style="text-align: center"><div>Obtain the most professional strategic plans and situational diagnoses online, in real time, at low prices and adapted to the needs of your brand or product.</div></article>
+			<?php } ?>
 
 			<section id="diseños">
 
