bdbc37b01e39351150eb498a942c18bef8d139c4
Ramon Ramos <ramonramospaz@gmail.com>
2017-02-28 12:36:01 -0400

    se coloca el link a capacidad de reaccion para que siga hasta el siguiente formulario

--- template-capacidad-reaccion.php
+++ template-capacidad-reaccion.php
@@ -214,6 +214,48 @@
 		}
 
 	}
+
+	if(isset($_POST["siguiente"]) && $_POST["siguiente"] == 'Siguiente'){
+
+		$query_progreso = mysqli_query($conexion, "SELECT progreso FROM wp_tmp_solicitud WHERE id_solicitud = '".$_SESSION["tmp_id_solicitud"]."'");
+
+		if(mysqli_num_rows($query_progreso) > 0){
+			$row_progreso = mysqli_fetch_assoc($query_progreso);
+			$progreso = $row_progreso["progreso"];
+			if($progreso !== ''){
+				$is_array = strpos($progreso, ',');
+				if($is_array !== false){
+					$progreso = explode(',',$row_progreso["progreso"]);
+					$progreso = array_unique($progreso);
+				}else{
+					$progreso = array($progreso);
+				}
+				
+				$find = in_array($_POST['idc'], $progreso);
+
+				if(!$find){
+					$progreso[] = $_POST['idc'];
+					$new_progreso = implode(",", $progreso);
+					$query_update_progreso = mysqli_query($conexion,"UPDATE wp_tmp_solicitud SET progreso ='".$new_progreso."' WHERE id_solicitud = '".$_SESSION["tmp_id_solicitud"]."'");
+				}
+			} else {
+				$query_update_progreso = mysqli_query($conexion,"UPDATE wp_tmp_solicitud SET progreso ='".$_POST['idc']."' WHERE id_solicitud = '".$_SESSION["tmp_id_solicitud"]."'");
+			}
+		}
+
+		$query_redireccion = mysqli_query($conexion, "SELECT enlace FROM wp_tmp_redireccion WHERE servicio_id = '".$_SESSION['tmp_id_servicio']."' AND tproducto LIKE '%".$_SESSION['tmp_t_producto']."%' AND tconsumidor LIKE '%".$_SESSION['tmp_t_consumidor']."%' AND cuestionario_id  = '".$_POST['idc']."'");
+
+		if(mysqli_num_rows($query_redireccion) > 0){
+
+			$row_redireccion = mysqli_fetch_assoc($query_redireccion);
+
+			$enlace = $row_redireccion["enlace"];
+
+			header("Location: ../".$enlace);
+
+		}
+
+	}
 	
  ?>
 
